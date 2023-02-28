d5c1fb0b69c2db2aae4aa106b1f225620c43e719
Juan Antonio Breña Moral <bren@juanantonio.info>
2023-02-28 13:00:22 +0100

    Moving topic to a commons place

--- consumer/src/main/java/info/jab/ms/service/KafkaListeners.java
+++ consumer/src/main/java/info/jab/ms/service/KafkaListeners.java
@@ -1,12 +1,15 @@
 package info.jab.ms.service;
 
+import info.jab.ms.config.KafkaCommons;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class KafkaListeners {
 
-    @KafkaListener(topics="example_topic2", groupId = "groupId")
+    private
+
+    @KafkaListener(topics = KafkaCommons.COMMON_TOPIC, groupId = "groupId")
     void listener(String data){
      System.out.println(("Listener received: "+ data + " "));
     }
--- producer/src/main/java/info/jab/ms/service/MyServiceImpl.java
+++ producer/src/main/java/info/jab/ms/service/MyServiceImpl.java
@@ -1,5 +1,6 @@
 package info.jab.ms.service;
 
+import info.jab.ms.config.KafkaCommons;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
@@ -7,13 +8,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class MyServiceImpl implements MyService {
 
-    private static final String TOPIC = "example_topic2";
-
     @Autowired
     private KafkaTemplate<String,String> kafkaTemplate;
 
     @Override
     public void send(String payload) {
-        kafkaTemplate.send(TOPIC, payload);
+        kafkaTemplate.send(KafkaCommons.COMMON_TOPIC, payload);
     }
 }
